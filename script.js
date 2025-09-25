8d3012b9ff66e6bf712ec8b95eacd329eb7f1e44
shristi Aswal <shristiaswal24@gmail.com>
2025-09-25 07:07:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function startDrill() {
+  alert("Virtual Drill Started! Follow on-screen instructions.");
+}
+
+function showBadge() {
+  document.getElementById("badge").classList.remove("hidden");
+}
